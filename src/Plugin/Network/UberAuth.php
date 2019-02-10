4e8b771b30293ecbd127228c2ad6925b65af4d4f
Getulio Sánchez <valentin2507@gmail.com>
2019-02-10 14:01:49 -0500

    Issue #3031970: Get callback URL from route

--- src/Plugin/Network/UberAuth.php
+++ src/Plugin/Network/UberAuth.php
@@ -2,17 +2,11 @@
 
 namespace Drupal\social_auth_uber\Plugin\Network;
 
-use Stevenmaguire\OAuth2\Client\Provider\Uber;
-use Drupal\Core\Config\ConfigFactoryInterface;
-use Drupal\Core\Entity\EntityTypeManagerInterface;
-use Drupal\Core\Logger\LoggerChannelFactoryInterface;
-use Drupal\Core\Routing\RequestContext;
-use Drupal\social_auth\SocialAuthDataHandler;
-use Drupal\social_api\Plugin\NetworkBase;
+use Drupal\Core\Url;
 use Drupal\social_api\SocialApiException;
+use Drupal\social_auth\Plugin\Network\NetworkBase;
 use Drupal\social_auth_uber\Settings\UberAuthSettings;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-use Drupal\Core\Site\Settings;
+use Stevenmaguire\OAuth2\Client\Provider\Uber;
 
 /**
  * Defines a Network Plugin for Social Auth Uber.
@@ -32,92 +26,6 @@ use Drupal\Core\Site\Settings;
  * )
  */
 class UberAuth extends NetworkBase implements UberAuthInterface {
-
-  /**
-   * The Social Auth Data Handler.
-   *
-   * @var \Drupal\social_auth\SocialAuthDataHandler
-   */
-  protected $dataHandler;
-
-  /**
-   * The logger factory.
-   *
-   * @var \Drupal\Core\Logger\LoggerChannelFactory
-   */
-  protected $loggerFactory;
-
-  /**
-   * The request context object.
-   *
-   * @var \Drupal\Core\Routing\RequestContext
-   */
-  protected $requestContext;
-
-  /**
-   * The site settings.
-   *
-   * @var \Drupal\Core\Site\Settings
-   */
-  protected $siteSettings;
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    return new static(
-      $container->get('social_auth.data_handler'),
-      $configuration,
-      $plugin_id,
-      $plugin_definition,
-      $container->get('entity_type.manager'),
-      $container->get('config.factory'),
-      $container->get('logger.factory'),
-      $container->get('router.request_context'),
-      $container->get('settings')
-    );
-  }
-
-  /**
-   * UberAuth constructor.
-   *
-   * @param \Drupal\social_auth\SocialAuthDataHandler $data_handler
-   *   The data handler.
-   * @param array $configuration
-   *   A configuration array containing information about the plugin instance.
-   * @param string $plugin_id
-   *   The plugin_id for the plugin instance.
-   * @param array $plugin_definition
-   *   The plugin implementation definition.
-   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
-   *   The entity type manager.
-   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
-   *   The configuration factory object.
-   * @param \Drupal\Core\Logger\LoggerChannelFactoryInterface $logger_factory
-   *   The logger factory.
-   * @param \Drupal\Core\Routing\RequestContext $requestContext
-   *   The Request Context Object.
-   * @param \Drupal\Core\Site\Settings $settings
-   *   The settings factory.
-   */
-  public function __construct(SocialAuthDataHandler $data_handler,
-                              array $configuration,
-                              $plugin_id,
-                              array $plugin_definition,
-                              EntityTypeManagerInterface $entity_type_manager,
-                              ConfigFactoryInterface $config_factory,
-                              LoggerChannelFactoryInterface $logger_factory,
-                              RequestContext $requestContext,
-                              Settings $settings
-  ) {
-
-    parent::__construct($configuration, $plugin_id, $plugin_definition, $entity_type_manager, $config_factory);
-
-    $this->dataHandler = $data_handler;
-    $this->loggerFactory = $logger_factory;
-    $this->requestContext = $requestContext;
-    $this->siteSettings = $settings;
-  }
 
   /**
    * Sets the underlying SDK library.
@@ -143,7 +51,7 @@ class UberAuth extends NetworkBase implements UberAuthInterface {
       $league_settings = [
         'clientId' => $settings->getClientId(),
         'clientSecret' => $settings->getClientSecret(),
-        'redirectUri' => $this->requestContext->getCompleteBaseUrl() . '/user/login/uber/callback',
+        'redirectUri' => Url::fromRoute('social_auth_uber.callback')->setAbsolute()->toString(),
       ];
 
       // Proxy configuration data for outward proxy.
@@ -175,6 +83,7 @@ class UberAuth extends NetworkBase implements UberAuthInterface {
       $this->loggerFactory
         ->get('social_auth_uber')
         ->error('Define Client ID and Client Secret on module settings.');
+
       return FALSE;
     }
 
